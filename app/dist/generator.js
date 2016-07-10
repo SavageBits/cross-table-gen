ad723ab9455bc90b9bd2a9e40e3a76eb2839b06d
Mark Savage <mark@savagebits.com>
2016-07-10 16:29:04 -0500

    styles close

--- app/dist/generator.js
+++ app/dist/generator.js
@@ -5,17 +5,18 @@ $.getJSON('/assets/data/sample.json', function (data) {
     $('#container').append('<p class=\'clear\'>');
     data.forEach(function (element) {
         var div = $('<div>', {
-            id: 'player' + element.id
+            id: 'player' + element.id,
+            class: 'player'
         });
         $('#container').append(div);
-        $(div).append('<div class=\'rank\'>' + element.rank + '</div>');
+        $(div).append('<div class=\'rank rank-row\'>' + element.rank + '</div>');
         $(div).append('<div class=\'name\'><span class=\'first-name\'>' + element.firstName + '</span>&nbsp;<span class=\'last-name\'>' + element.lastName + '</span></div>');
         $(div).append('<div class=\'rating\'>' + element.rating + '</div>');
         for (var i = 0; i < numberOfPlayers; i++) {
             if (element.rank === i + 1)
-                $(div).append('<div class=\'rank match-score\'>X</div>');
+                $(div).append('<div class=\'rank match-score self\'>X</div>');
             else
-                $(div).append('<div class=\'rank match-score\'>' + getMatchScore(element.id, data[i].id) + '</div>');
+                $(div).append('<div class=\'rank match-score\'>' + getMatchPoints(data, element.id, data[i].id) + '</div>');
         }
         $(div).append('<p class=\'clear\'>');
     });
@@ -27,9 +28,24 @@ function sortPlayers(data) {
 }
 function printMatchColumnHeaders(numberOfPlayers) {
     for (var i = 1; i <= numberOfPlayers; i++) {
-        $('#header').append('<div class=\'rank column\'>' + i + '</div>');
+        var alternateClass = i % 2 == 0 ? '' : 'rank-alternate';
+        $('#header').append('<div class=\'rank ' + alternateClass + ' column\'>' + i + '</div>');
     }
 }
-function getMatchScore(player1, player2) {
-    return 1;
+function getMatchPoints(data, player1Id, player2Id) {
+    var player1 = findPlayer(data, player1Id);
+    var matchPoints = null;
+    for (var i = 0; i < player1.matches.length; i++)
+        if (player1.matches[i].playerId == player2Id)
+            matchPoints = player1.matches[i].points;
+    if (matchPoints == 0.5)
+        matchPoints = '1/2';
+    return matchPoints == null ? '' : matchPoints;
+}
+function findPlayer(data, playerId) {
+    var player = {};
+    for (var i = 0; i < data.length; i++)
+        if (data[i].id === playerId)
+            player = data[i];
+    return player;
 }
--- app/src/generator.ts
+++ app/src/generator.ts
@@ -10,20 +10,21 @@ $.getJSON('/assets/data/sample.json', function(data) {
   
   data.forEach(element => {
     let div = $('<div>', {
-      id: 'player' + element.id
+      id: 'player' + element.id,
+      class: 'player'
     });
     
     $('#container').append(div);
     
-    $(div).append('<div class=\'rank\'>' + element.rank + '</div>'); //rank should be derived, similar to score and based on score
+    $(div).append('<div class=\'rank rank-row\'>' + element.rank + '</div>'); //rank should be derived, similar to score and based on score
     $(div).append('<div class=\'name\'><span class=\'first-name\'>' + element.firstName + '</span>&nbsp;<span class=\'last-name\'>' + element.lastName + '</span></div>');
     $(div).append('<div class=\'rating\'>' + element.rating + '</div>');
 
     for (var i = 0; i < numberOfPlayers; i++) {
       if (element.rank === i + 1)
-        $(div).append('<div class=\'rank match-score\'>X</div>'); 
+        $(div).append('<div class=\'rank match-score self\'>X</div>'); 
       else
-        $(div).append('<div class=\'rank match-score\'>' + getMatchScore(element.id, data[i].id) + '</div>');
+        $(div).append('<div class=\'rank match-score\'>' + getMatchPoints(data, element.id, data[i].id) + '</div>');
     }
 
     $(div).append('<p class=\'clear\'>');
@@ -40,10 +41,33 @@ function sortPlayers(data) {
 
 function printMatchColumnHeaders(numberOfPlayers: number) {
   for (var i = 1; i <= numberOfPlayers; i++) {
-    $('#header').append('<div class=\'rank column\'>' + i + '</div>');    
+    var alternateClass = i % 2 == 0 ? '' : 'rank-alternate';
+    $('#header').append('<div class=\'rank ' + alternateClass + ' column\'>' + i + '</div>');    
   }
 }
 
-function getMatchScore(player1, player2) {
-  return 1;
+function getMatchPoints(data, player1Id, player2Id) {
+  var player1 = findPlayer(data, player1Id);
+  
+  var matchPoints = null;
+
+  for (var i = 0; i < player1.matches.length; i++) 
+    if (player1.matches[i].playerId == player2Id) 
+      matchPoints = player1.matches[i].points;
+
+  if (matchPoints == 0.5) 
+    matchPoints = '1/2';
+
+  return matchPoints == null ? '' : matchPoints;
 }
+
+function findPlayer(data, playerId) {
+  let player = {};
+
+  for (var i = 0; i < data.length; i++) 
+     if (data[i].id === playerId)
+       player = data[i];
+
+  return player;
+}
+
